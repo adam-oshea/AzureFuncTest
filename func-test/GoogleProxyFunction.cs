74575f3e1e36261b68ea42d47e49b346a1b6cfde
adam-oshea <adam.oshea@bearingpoint.com>
2026-01-28 16:05:58 +0000

    added func

---
+++ func-test/GoogleProxyFunction.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+
+public class GoogleProxyFunction
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILogger _logger;
+
+    public GoogleProxyFunction(IHttpClientFactory httpClientFactory, ILoggerFactory loggerFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+        _logger = loggerFactory.CreateLogger<GoogleProxyFunction>();
+    }
+
+    [Function("GoogleProxy")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+    {
+        _logger.LogInformation("Calling google.ie");
+
+        var googleResponse = await _httpClient.GetAsync("https://www.google.ie");
+        var content = await googleResponse.Content.ReadAsStringAsync();
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/html; charset=utf-8");
+        await response.WriteStringAsync(content);
+
+        return response;
+    }
+}
